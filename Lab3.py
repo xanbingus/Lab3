ee2e945ad444c1916236cb103d0c4305ba036afa
Xan <xantan.21@ichat.sp.edu.sg>
2022-11-04 16:37:01 +0800

    add new files.

--- Lab3.py
+++ Lab3.py
@@ -12,7 +12,15 @@ def bubble_sort(arr, sorting_order):
     # Get number of elements in the list
     n = len(arr_result)
 
-    if n < 10:
+    if n > 10:
+        return 1
+    elif n == 0:
+        return 0
+    elif n < 10:
+        for i in arr_result:
+            if type(i) != int:
+                return 2
+    elif n < 10:
         # Traverse through all array elements
         for i in range(n - 1):
             # range(n) also work but outer loop will
@@ -32,7 +40,13 @@ def bubble_sort(arr, sorting_order):
 
                 else:
                     # Return an empty array
+
+
                     arr_result = []
+
+    elif n==0:
+        return 0
+
     else:
         arr_result = -1
 
