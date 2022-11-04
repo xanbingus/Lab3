a988c4faf7e1229727bb6de8f1687a1de2053d75
Xan <xantan.21@ichat.sp.edu.sg>
2022-11-04 16:04:54 +0800

    v1.0

---
+++ Test_Lab3.py
@@ -0,0 +1,51 @@
+import Lab3
+
+print("Test_Lab3")
+
+
+def test_bubble_sort_ascending():
+    result = []
+    input_arr = [64, 34, 25, 12, 22, 11, 90]
+    test_arr = [11, 12, 22, 25, 34, 64, 90]
+
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
+
+    assert (result == test_arr)
+
+def test_bubble_sort_descending():
+    result = []
+    input_arr = [64, 34, 25, 12, 22, 11, 90]
+    test_arr = [90, 64, 34, 25, 22, 12, 11]
+
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_DESCENDING)
+
+    assert (result == test_arr)
+
+def test_bubble_sort_invalid():
+    result = []
+    input_arr = [64, 34, 25, 12, 22, 11, 90]
+
+    result = Lab3.bubble_sort(input_arr, 3)
+
+    assert (result == [])
+
+def test_bubble_sort_over():
+    result = []
+    input_arr = [1,2,3,4,5,6,7,8,9,10,11]
+
+    result = Lab3.bubble_sort(input_arr,Lab3.SORT_ASCENDING)
+    assert (result == 1)
+
+def test_bubble_sort_empty():
+    result = []
+    input_arr = []
+
+    result = Lab3.bubble_sort(input_arr,Lab3.SORT_ASCENDING)
+    assert (result ==0)
+
+def test_bubble_sort_integer():
+    result = []
+    input_arr = [1,2,3,'a']
+
+    result = Lab3.bubble_sort(input_arr,Lab3.SORT_ASCENDING)
+    assert (result==2)
